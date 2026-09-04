186f339822a8d170ae42180f6030f31a326ba6d0
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-04 10:40:16 +0700

    Update

---
+++ main3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool SoNguyenTo (int n){
+    if (n <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++){
+        if (n % i == 0){
+            return false;
+        }
+    }
+    return true;
+
+}
+
+int main (){
+    int n;
+    cout << "Nhap so n: ";
+    cin >> n;
+
+    if (SoNguyenTo (n)){
+        cout << n << " la so nguyen to" << endl;
+    }
+    else cout << n << " khong phai la so nguyen to" << endl; 
+
+    return 0;
+}
