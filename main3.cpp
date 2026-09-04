97f077d94e7fb2cba1e8a8cdc8781011dabf444a
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-04 10:41:31 +0700

    Update

--- main3.cpp
+++ main3.cpp
@@ -7,7 +7,7 @@ bool SoNguyenTo (int n){
         return false;
     }
 
-    for (int i = 2; i <= sqrt(n); i++){
+    for (int i = 2; i <= n; i++){
         if (n % i == 0){
             return false;
         }
