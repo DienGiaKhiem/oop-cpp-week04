f94d0e7b9fa3ab5f052d8549e1b5b1e9a356d432
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-04 09:57:13 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,35 +1,15 @@
 #include <iostream>
 #include <string>
-using namespace std; 
+using namespace std;
+void printname(int n, string name){
+    getline (cin, name);
+cout << "Name: " << name << endl;
+}
+
 int main()
 {
-    std::cout << "Hello, C++!" << std::endl;
+    string name;
+    printname(1, name);
     return 0;
 }
-
-void inputStudent(){
-    int n = 0; 
-    string names[20];
-    string ids[20];
-    string phones[20]; 
-    std :: cout << "Input the number of students:" << std::endl;
-    std :: cin >> n; 
-
-    if (n>0){
-        for (int i = 0; i<n;i++){
-            cout << "Enter information of student " << i + 1;
-            cout << "Name:";
-            cin >> names[i]; 
-            cout << "ID: ";
-            cin >> ids[i];
-            cout << "Phone : ";
-            cin >> phones[i];
-        }
-        cout << "Number of inputted students: " << n;
-
-    }
-    else{
-        cout << "The number of students must be more than 0";
-    }
-    
-}
+//kiểm tra gits
